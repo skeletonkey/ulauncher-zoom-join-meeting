02ba7e8649925646219c8ce22c8951080ab6593d
Erik Tank <lefthandskeletonkey@gmail.com>
2020-04-12 19:05:10 -0700

    First working history.
    This works - as long as you don't reboot ... unless it get slow - then you have too many zoom ids
    in the history array.
    
    At this time there is no protections - EI clearing of history of unused IDs, ordering IDs by most used,
    perserving IDs between restarts.

--- main.py
+++ main.py
@@ -1,46 +1,126 @@
-from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.shared.action.ActionList import ActionList
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.event import ItemEnterEvent, KeywordQueryEvent, PreferencesEvent, PreferencesUpdateEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 
 import logging
 import re
 import sys
 
+ZOOM_ICON = 'images/zoom_icon.png'
+ERROR_ICON = 'images/error_icon.png',
+
 logger = logging.getLogger(__name__)
 shortcuts = {}
-history = {}
-
-def updateShortcuts(shortcutString):
-    shortcuts.clear
-    logger.info("Updating Shortcuts - %s" % (shortcutString))
-    shortcutPairs = shortcutString
-    for pair in shortcutPairs.split(';'):
-        keyValue = pair.split(':')
-        shortcuts[keyValue[0]] = keyValue[1]
-        logger.debug("Shortcut %s for %s" % (keyValue[0], keyValue[1]))
-
-def checkForShortcut(string):
-    if string in shortcuts:
-        logger.info("Shortcut found!  %s converted to %s" % (string, shortcuts[string]))
-        return shortcuts[string]
-    else:
-        return string
+id_history = []
 
 class ZoomJoinMeeting(Extension):
     def __init__(self):
         super(ZoomJoinMeeting, self).__init__()
-        self.subscribe(ItemEnterEvent, UserHitEnterListener())
+        self.subscribe(ItemEnterEvent, LoadNewZoom())
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
         self.subscribe(PreferencesEvent, PreferencesLoadListener())
         self.subscribe(PreferencesUpdateEvent, PreferencesUpdateListener())
 
-class UserHitEnterListener(EventListener):
+def updateShortcuts(shortcutString):
+    shortcuts.clear
+    logger.info("Updating Shortcuts - %s" % (shortcutString))
+    shortcut_pairs = shortcutString
+    for pair in shortcut_pairs.split(';'):
+        key_value = pair.split(':')
+        shortcuts[key_value[0]] = key_value[1]
+        logger.debug("Shortcut %s for %s" % (key_value[0], key_value[1]))
+
+class Zoom:
+    def __init__(self, id, base_uri):
+        self.base_uri = base_uri
+        self.id = id
+        self.type = ''
+
+        # Fill out the object with extra data - order matters!!!
+        self.checkForShortcut()
+        self.determineLinkType()
+
+    def determineLinkType(self):
+        logger.info("determineLinkType for %s" % (self.id))
+
+        # Zoom offers two types of links:
+        #  1) Meeting ID which is only numbers ('j')
+        #  2) Personal Link which is 5-40 characters, start with a letter, and contain only a-z, 0-9, and '.' ('my')
+        if re.match('^\d+$', self.id):
+            self.type = 'j'
+        elif re.match('^[a-z][a-z0-9.]{4,39}$', self.id):
+            self.type = 'my'
+
+        logger.info("Found that %s is %s" % (self.id, self.type))
+
+    def checkForShortcut(self):
+        if self.id in shortcuts:
+            logger.info("Shortcut found!  %s converted to %s" % (self.id, shortcuts[self.id]))
+            self.id = shortcuts[self.id]
+
+    def fullUri(self):
+        return "https://" + self.base_uri + '/' + self.type + '/' + self.id
+
+    def validate(self):
+        logger.info("Validating Zoom ID: %s" % (self.id))
+        if self.id and self.type:
+            return True
+        else:
+            return False
+
+class History:
+    def __init__(self, max_matches, enabled):
+        if (enabled == 'yes'):
+            self.enabled = True
+        else:
+            self.enabled = False
+
+        if (int(max_matches) > 0):
+            self.max_matches = int(max_matches)
+        else:
+            logger.info("max_matches either not set or wasn't understood as an integer - falling back to default of 5")
+            self.max_matches = 5
+
+    def getMatching(self, start):
+        if not self.enabled:
+            return []
+
+        length_of_start = len(start)
+        matching = []
+        logger.info("Looking to match %s" % (start))
+        matches_found = 0
+        for id in id_history:
+            logger.info("Found ID: %s -- %s" % (id, id[:length_of_start]))
+            if id[:length_of_start] == start:
+                logger.info("Found match - adding %s" % (id))
+                matches_found += 1
+                matching.append(id)
+                if matches_found >= self.max_matches:
+                    break
+
+        return matching
+
+    def remember(self, id):
+        logger.info("History may remember this!")
+        if self.enabled:
+            logger.info("Checking to see if we need to remember %s" % (id))
+            if not id in id_history:
+                logger.info("Adding id to list -- %s" % (id))
+                id_history.append(id)
+
+class LoadNewZoom(EventListener):
     def on_event(self, event, extension):
-        zoomId = event.get_data()
-        logger.info("User entered zoom id: %s" % (zoomId))
+        data = event.get_data()
+        history = History(
+            enabled = extension.preferences['history'],
+            max_matches = extension.preferences['history_count'],
+        )
+        history.remember(data['id'])
 
 class PreferencesLoadListener(EventListener):
     def on_event(self, event, extension):
@@ -51,76 +131,48 @@ class PreferencesUpdateListener(EventListener):
         if event.id == 'shortcuts':
             updateShortcuts(event.new_value)
 
-
 class KeywordQueryEventListener(EventListener):
-    def determineLinkType(self, zoomId):
-        logger.info("determineLinkType for %s" % (zoomId))
-        chatType = ''
-
-        # Zoom offers two types of links:
-        #  1) Meeting ID which is only numbers ('j')
-        #  2) Personal Link which is 5-40 characters, start with a letter, and contain only a-z, 0-9, and '.' ('my')
-        if re.match('^\d+$', zoomId):
-            chatType = 'j'
-        elif re.match('^[a-z][a-z0-9.]{4,39}$', zoomId):
-            chatType = 'my'
-
-        logger.info("Found that %s is %s" % (zoomId, chatType))
-        return chatType
-
     def on_event(self, event, extension):
-        baseUri = extension.preferences['base_uri']
-        linkDeterminationType = extension.preferences['link_type']
+        chat_id = event.get_query()[len(extension.preferences['zoom_kw']) + 1:]
+        logger.info("User Input: %s" % (chat_id))
 
-        userInputs = event.get_query().split()
-        logger.info("User Inputs: " + "--".join(userInputs))
+        history = History(
+            enabled = extension.preferences['history'],
+            max_matches = extension.preferences['history_count'],
+        )
 
-        chatType = 'j'
-        chatId = ''
-        error = 1
+        zoom = None
+        if chat_id:
+            zoom = Zoom(id = chat_id, base_uri = extension.preferences['base_uri'])
 
-        if linkDeterminationType == 'auto':
-            if len(userInputs) == 2:
-                chatId = checkForShortcut(userInputs[1])
-                chatType = self.determineLinkType(chatId)
-                if len(chatType) > 0:
-                    error = 0
+        result_items = []
+        if not zoom or not zoom.validate():
+            result_items.append(ExtensionResultItem(
+                icon = ERROR_ICON,
+                name = 'Unable to verify Zoom ID: ' + chat_id
+            ))
         else:
-            if len(userInputs)  == 3:
-                if userInputs[1] == 'my':
-                    chatType = 'my'
-                chatId = checkForShortcut(userInputs[2])
-                error = 0
-            elif len(userInputs) == 2:
-                chatId = checkForShortcut(userInputs[1])
-                error = 0
+            result_items.append(ExtensionResultItem(
+                icon = ZOOM_ICON,
+                name = 'Open Zoom for: ' + zoom.fullUri(),
+                on_enter = ActionList([
+                    ExtensionCustomAction({ "id": zoom.id, "url": zoom.fullUri() }),
+                    OpenUrlAction(zoom.fullUri())
+                ])
+            ))
 
-            if chatType != self.determineLinkType(chatId):
-                error = 1
+            # We use chat_id instead of zoom.id here since chat_id what the user entered and not a shortcut
+            history_items = history.getMatching(chat_id)
+            for item in history_items:
+                zoom = Zoom(id = item, base_uri = extension.preferences['base_uri'])
+                result_items.append(ExtensionResultItem(
+                    icon = ZOOM_ICON,
+                    name = 'Open Zoom for: ' + zoom.fullUri(),
+                    on_enter = OpenUrlAction(zoom.fullUri())
+                ))
 
-        resultItem = None
-        if error and len(chatId):
-            resultItem = ExtensionResultItem(
-                icon = 'images/error_icon.png',
-                name = 'Unable to verify Zoom ID: ' + chatId
-            )
-        elif len(chatId):
-            fullUri = "https://" + baseUri + '/' + chatType + '/' + chatId
-            resultItem = ExtensionResultItem(
-                icon = 'images/zoom_icon.png',
-                name = 'Open Zoom for: ' + fullUri,
-                on_enter = OpenUrlAction(fullUri)
-            )
-
-        historyItems = []
-        if extension.preferences['history'] == 'yes':
-            historyItems = getHistoricalItems(userInputs, extension.preferences['history_count'])
-
-        return RenderResultListAction([resultItem])
-
-    def getHistoricalItems(userInputs, count):
-        return []
-
+        return RenderResultListAction(result_items)
 
 if __name__ == '__main__':
     ZoomJoinMeeting().run()
+
